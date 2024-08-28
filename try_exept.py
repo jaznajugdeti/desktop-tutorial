24b0b36fb2dbc94158f8a0e898a2bda323f84a90
jaznajugdeti <71246900+jaznajugdeti@users.noreply.github.com>
2024-08-28 18:54:23 +0300

    Update try_exept.py

--- try_exept.py
+++ try_exept.py
@@ -1,11 +1,12 @@
 
 try:
     def add_everything_up(a,b):
-        sum_ = a +b
+        try:
+         print(a + b)
         print(sum_)
 
 except TypeError:
-    print(a, b, sep='')
+    print(str(a)+str(b))
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
