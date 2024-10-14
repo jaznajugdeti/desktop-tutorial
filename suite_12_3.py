10cc14a148c3f5471a71ba8909aa884e2c832723
jaznajugdeti <71246900+jaznajugdeti@users.noreply.github.com>
2024-10-14 20:53:42 +0300

    Update suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -2,8 +2,6 @@ import unittest
 from test_12__3 import TournamentTest, RunnerTest
 
 # Создайте объект класса TextTestRunner, с аргументом verbosity=2.
-test_run = unittest.TestSuite()
-
 # Классы RunnerTest дополнить атрибутом is_frozen = False и TournamentTest атрибутом is_frozen = True.
 # # Напишите соответствующий декоратор к каждому методу (кроме @classmethod), который при значении
 # # is_frozen = False будет выполнять тесты, а is_frozen = True - пропускать и выводить сообщение
@@ -13,4 +11,4 @@ test_run_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))
 test_run_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))
 
 runner = unittest.TextTestRunner(verbosity=2)
-runner.run(test_run)
+runner.run(test_run_suite)
